ac38151ddbe2d656036659888c54e4902587094a
raoulvdberge <raoulvdberge@gmail.com>
2024-07-07 02:32:17 +0200

    chore: upgrade refined architect

---
+++ refinedstorage-query-parser/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("refinedarchitect.base")
+}
+
+refinedarchitect {
+    testing()
+    mutationTesting()
+    javadoc()
+    publishing {
+        maven = true
+    }
+}
+
+base {
+    archivesName.set("refinedstorage-query-parser")
+}
+
+dependencies {
+    api(project(":refinedstorage-core-api"))
+    testImplementation(libs.junit.api)
+    testImplementation(libs.junit.params)
+    testImplementation(libs.assertj)
+    testRuntimeOnly(libs.junit.engine)
+}
